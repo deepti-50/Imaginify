b158e54c72adcf136f60c676eb4cd86aecca1604
Deepti <deeptisunilsingh50@gmail.com>
2024-12-17 17:08:01 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const  Transformations = () => {
+  return (
+    <div>Transformations</div>
+  )
+}
+
+export default Transformations
+
+//localhost:3000/transformations
+//localhost:3000/transformations/[id]
+ 
