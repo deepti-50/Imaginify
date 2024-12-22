d6ad7c4bd10f2e9756dcd723b9b0d58c2eefebe5
Deepti <deeptisunilsingh50@gmail.com>
2024-12-22 18:35:19 +0530

    Added different files for its functionalities

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,33 +7,26 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-// Using a global declaration to properly type the global object
-declare global {
-  var mongoose: MongooseConnection | undefined;
-}
+let cached: MongooseConnection = (global as any).mongoose
 
-// Ensure that `cached` has a proper type
-let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
-
-// If `global.mongoose` doesn't exist, initialize it
-if (!global.mongoose) {
-  global.mongoose = cached;
-}
-
-export const connectToDatabase = async (): Promise<Mongoose> => {
-  if (cached.conn) return cached.conn;
-
-  if (!MONGODB_URL) {
-    throw new Error('Missing MONGODB_URL');
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
   }
+}
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'imaginify',
-      bufferCommands: false,
-    });
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
+
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
 
   cached.conn = await cached.promise;
+
   return cached.conn;
-};
+}
